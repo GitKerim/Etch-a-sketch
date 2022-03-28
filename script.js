ed11ccb56b3c96a5e04862283fbf83f5cb567db9
Kerim <kerimomerbegovic@hotmail.com>
2022-03-28 22:04:22 +0200

    Added working buttons for black and rainbow brush

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 
-let resetButton = document.querySelector('.navigation > button');
+let resetButton = document.querySelector('#clearbutton');
 let title = document.querySelector('header > p');
 let container = document.querySelector('#container');  
 let slider = document.getElementById('slider');
 let valueSpan = document.getElementById('value');
 let grid = slider.value;
+let blackBrush = document.querySelector('.blackBrush');
+let shader = document.querySelector('.shader');
+let eraser = document.querySelector('.eraser');
+let rainbow = document.querySelector('.rainbow')
+
 
 
 function createGrid(){
@@ -14,7 +19,16 @@ function createGrid(){
     let sizeValues = Number(1/grid).toLocaleString(undefined,{style: 'percent', minimumFractionDigits:5}); 
     box.setAttribute('id', 'box');
     box.style.setProperty('--value', sizeValues);
-    box.addEventListener('mouseover', () => box.style.backgroundColor = randomColor())
+    box.addEventListener('mouseover', () => {
+        box.style.backgroundColor = "black";
+        box.style.borderColor = "white";    
+    });
+    rainbow.addEventListener('click',() => {
+        box.addEventListener('mouseover', () => {
+            box.style.backgroundColor = randomColor();
+            box.style.borderColor = "black";});  
+    });
+    
     container.appendChild(box); 
 };
 }
@@ -34,13 +48,21 @@ resetButton.addEventListener('click', () => {
     title.style.color = 'white';
     grid = 16;
 });
+
+function brush(){
+
+    let colorBlack = "rgb(0,0,0)";
+    return colorBlack;
+    
+    
+}
 function randomColor() {
     let r = Math.round( Math.random() * 255);
     let g = Math.round( Math.random() * 255);
     let b = Math.round( Math.random() * 255);
     let colorString = "rgb(" + r + "," + g + "," + b + ")";
-    return colorString;
-}
+    return colorString;}
+    
 
 title.addEventListener('mouseover', () => title.style.color = randomColor());
 
