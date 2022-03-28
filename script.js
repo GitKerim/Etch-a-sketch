369e8e75b2a3d684bfa85ba86a1957c4b735f1b0
Kerim <kerimomerbegovic@hotmail.com>
2022-03-28 23:25:10 +0200

    functioning buttons for black, shade and eraser

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 
-let resetButton = document.querySelector('#clearbutton');
+let resetButton = document.querySelector('.clearbutton');
 let title = document.querySelector('header > p');
 let container = document.querySelector('#container');  
 let slider = document.getElementById('slider');
 let valueSpan = document.getElementById('value');
 let grid = slider.value;
-let blackBrush = document.querySelector('.blackBrush');
-let shader = document.querySelector('.shader');
-let eraser = document.querySelector('.eraser');
-let rainbow = document.querySelector('.rainbow')
+let buttons = document.querySelectorAll('.buttons')
 
 
+let color = "black";
 
 function createGrid(){
     container.innerHTML = ''
@@ -19,18 +17,46 @@ function createGrid(){
     let sizeValues = Number(1/grid).toLocaleString(undefined,{style: 'percent', minimumFractionDigits:5}); 
     box.setAttribute('id', 'box');
     box.style.setProperty('--value', sizeValues);
-    box.addEventListener('mouseover', () => {
-        box.style.backgroundColor = "black";
-        box.style.borderColor = "white";    
-    });
-    rainbow.addEventListener('click',() => {
-        box.addEventListener('mouseover', () => {
-            box.style.backgroundColor = randomColor();
-            box.style.borderColor = "black";});  
-    });
-    
     container.appendChild(box); 
 };
+let boxes = container.querySelectorAll('div');
+boxes.forEach(box => box.addEventListener('mouseover', brushColor));
+}
+
+function brushColor(){
+    switch (color){
+        case 'rainbow':
+            this.style.backgroundColor = randomColor();
+            break;
+        case 'shader':
+            this.style.backgroundColor = "gray";
+            break;
+        case 'eraser':
+            this.style.backgroundColor = "white";
+            break;
+        default:
+            this.style.backgroundColor = "black";
+            break;
+
+
+    }
+}
+
+function changeColor(event) {
+    switch (event.target.dataset.color) { 
+        case 'rainbow':
+            color = 'rainbow';
+            break;  
+        case 'shader':
+            color = 'shader';
+            break;
+        case 'eraser':
+            color = 'eraser';
+            break;
+        default:
+            color = 'black';
+            break;
+    } 
 }
 
 let update = () => {
@@ -39,7 +65,6 @@ let update = () => {
     createGrid();
 }
 slider.addEventListener('input', update);
-update();
 
 
 resetButton.addEventListener('click', () => {
@@ -49,13 +74,6 @@ resetButton.addEventListener('click', () => {
     grid = 16;
 });
 
-function brush(){
-
-    let colorBlack = "rgb(0,0,0)";
-    return colorBlack;
-    
-    
-}
 function randomColor() {
     let r = Math.round( Math.random() * 255);
     let g = Math.round( Math.random() * 255);
@@ -64,8 +82,7 @@ function randomColor() {
     return colorString;}
     
 
-title.addEventListener('mouseover', () => title.style.color = randomColor());
-
 window.onload = ()=> {
     createGrid();
 }
+buttons.forEach(button => button.addEventListener('click', changeColor));
